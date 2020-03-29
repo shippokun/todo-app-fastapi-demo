b9e92f8827ae46517d99ed31b6e23f9660a1c3e4
shippokun <okashi0tabetai@gmail.com>
2020-03-30 00:16:24 +0900

    feat: add frontend directory

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <b-form-input
+      v-model="text"
+      placeholder="Enter Task abot content"
+    ></b-form-input>
+    <div class="mt-2">Value: {{ text }}</div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  data() {
+    return {
+      text: ''
+    }
+  }
+  // components: {
+  //   HelloWorld
+  // }
+};
+</script>
